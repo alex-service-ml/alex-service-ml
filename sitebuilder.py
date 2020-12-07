5f31988246ddfe5be09667ab860525a627354c91
Alex Service <alex.service@gmail.com>
2020-12-07 15:05:52 -0500

    Github pages should hopefully serve bootstrap locally now

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import os
 
 from flask import Flask, render_template, url_for
 from flask_flatpages import FlatPages
@@ -46,10 +47,22 @@ def parse_args():
     parser.add_argument('--build', action='store_true', help='Builds static html pages from readmes')
     return parser.parse_args()
 
+def build_site():
+    freezer.freeze()
+    
+    # Github pages CSS fix
+    # adds project name to local path for bootstrap-flask
+    for pagename in os.listdir(FREEZER_DESTINATION):
+        if pagename.endswith('.html'):
+            with open(os.path.join(FREEZER_DESTINATION, pagename)) as f:
+                contents = f.read().replace('/bootstrap/static/', '/alex-service-ml/bootstrap/static/')
+            with open(os.path.join(FREEZER_DESTINATION, pagename), 'w') as f:
+                f.write(contents)
+
 if __name__ == '__main__':
     args = parse_args()
     if args.build:
-        freezer.freeze()
+        build_site()
     else:
         app.run(host='0.0.0.0', port=5000)
 
